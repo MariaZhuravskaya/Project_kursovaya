ff0985db0d0a5a561a0aa120e2bfbfef87e31d24
Maria <maria.mandrykina@gmail.com>
2023-06-05 15:38:18 +0300

    test_strftime

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -7,3 +7,9 @@ def test_datetime():
     assert functions.get_datetime("2018-03-09T23:57:37.537412") == datetime.fromisoformat("2018-03-09 23:57:37.537412")
     assert functions.get_datetime("2018-12-22T02:02:49.564873") == datetime.fromisoformat("2018-12-22 02:02:49.564873")
 
+
+def test_strftime():
+    assert functions.get_strftime("2019-01-05T00:52:30.108534") == '05.01.2019'
+    assert functions.get_strftime("2018-03-09T23:57:37.537412") == '09.03.2018'
+    assert functions.get_strftime("2018-12-22T02:02:49.564873") == '22.12.2018'
+
