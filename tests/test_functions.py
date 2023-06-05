df37812c772128f02d2b37ac4deee1a44f90afdf
Maria <maria.mandrykina@gmail.com>
2023-06-05 15:38:38 +0300

    test_from_card

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -13,3 +13,9 @@ def test_strftime():
     assert functions.get_strftime("2018-03-09T23:57:37.537412") == '09.03.2018'
     assert functions.get_strftime("2018-12-22T02:02:49.564873") == '22.12.2018'
 
+
+
+def test_from_card():
+    assert functions.get_from("MasterCard 6783917276771847") == "MasterCard 6783 91** **** 1847"
+    assert functions.get_from("Visa Gold 8326537236216459") == "Visa Gold 8326 53** **** 6459"
+    assert functions.get_from("МИР 5211277418228469") == "МИР 5211 27** **** 8469"
